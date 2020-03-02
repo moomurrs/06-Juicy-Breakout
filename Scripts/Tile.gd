693a4a5e41e79fad7709ac77ccdfa0bb1fee6140
moomurrs <59849596+moomurrs@users.noreply.github.com>
2020-03-02 11:51:24 -0500

    tile tween

--- Scripts/Tile.gd
+++ Scripts/Tile.gd
@@ -2,5 +2,16 @@ extends StaticBody2D
 
 var points = 10
 
+onready var _target = position
+onready var t = $Timer
+
 func _ready():
-	pass
+	position.y = -30
+	var time = rand_range(0, 2.5)
+	t.set_wait_time(time)
+	t.start()
+	# do not do anything else in this scipt until this single tile has timeout
+	yield(t, "timeout")
+	# after timeout, interpolate tween on this one tile
+	$Tween.interpolate_property(self, "position", position, _target, 2.0, Tween.TRANS_BOUNCE,Tween.EASE_OUT)
+	$Tween.start()
